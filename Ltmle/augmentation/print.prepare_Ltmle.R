6bcb9ca9cf94ed7183c80fd91a8770627aa17e8b
Thomas Alexander Gerds <tag@biostat.ku.dk>
2023-05-19 16:44:07 +0200

    summary fun

---
+++ Ltmle/augmentation/print.prepare_Ltmle.R
@@ -0,0 +1,23 @@
+### print.prepare_Ltmle.R --- 
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: May 19 2023 (15:57) 
+## Version: 
+## Last-Updated: May 19 2023 (16:00) 
+##           By: Thomas Alexander Gerds
+##     Update #: 3
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+print.prepare_Ltmle <- function(x,...){
+    cat("\nObject ready for analysis with Ltmle.\n")
+}
+
+
+######################################################################
+### print.prepare_Ltmle.R ends here
