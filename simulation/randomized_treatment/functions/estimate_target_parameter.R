852bf660f70e8c2ae452c2f45ce765649a3da31d
Thomas Alexander Gerds <tag@biostat.ku.dk>
2023-10-16 16:03:13 +0200

    now starting AB simus

--- simulation/randomized_treatment/functions/estimate_target_parameter.R
+++ simulation/randomized_treatment/functions/estimate_target_parameter.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Sep 21 2023 (15:34) 
 ## Version: 
-## Last-Updated: Sep 28 2023 (08:55) 
+## Last-Updated: Oct 16 2023 (16:02) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 4
+##     Update #: 5
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -15,42 +15,18 @@
 ## 
 ### Code:
 estimate_target_parameter <- function(){
-    d = simulate_data(n = 200,treatment_effect = 0.1,covariate_effect = c(0.7,0))
-    d[,{
-        fit.unadjusted <- Ltmle(data = d[,.(W,A,Y)],
-                                time_horizon = 1,
-                                estimate.time = FALSE,
-                                gbounds = c(0,1),
-                                Lnodes = "W",
-                                survivalOutcome = FALSE,
-                                Qform = c("Y" = "Q.kplus1 ~ W + A"),
-                                gform = "A~1",
-                                Anodes = "A",
-                                Ynodes = "Y",
-                                verbose = FALSE,
-                                abar = list(0,1))
-        fit.adjusted <- Ltmle(data = d[,.(W,A,Y)],
-                              time_horizon = 1,
-                              estimate.time = FALSE,
-                              gbounds = c(0,1),
-                              Lnodes = "W",
-                              survivalOutcome = FALSE,
-                              Qform = c("Y" = "Q.kplus1 ~ W + A"),
-                              gform = "A~W",
-                              Anodes = "A",
-                              Ynodes = "Y",
-                              verbose = FALSE,
-                              abar = list(0,1))
-        sum.unadjusted <- summary.Ltmle(fit.unadjusted)
-        sum.adjusted <- summary.Ltmle(fit.adjusted)
-        .(Estimate = c("Hypothetical","Naive","Propensity unadjusted","Propensity adjusted"),
-          Risk_treated = c(mean(Y_treated),mean(Y[A == 1]),sum.unadjusted[Target_parameter == "Mean(A=1)"]$estimate,sum.adjusted[Target_parameter == "Mean(A=1)"]$estimate),
-          Risk_untreated = c(mean(Y_untreated),mean(Y[A == 0]),sum.unadjusted[Target_parameter == "Mean(A=0)"]$estimate,sum.adjusted[Target_parameter == "Mean(A=0)"]$estimate),
-          ATE = c(mean(Y_treated)-mean(Y_untreated),
-                  mean(Y[A == 1])-mean(Y[A == 0]),
-                  sum.unadjusted[Target_parameter == "ATE"]$estimate,
-                  sum.adjusted[Target_parameter == "ATE"]$estimate))
-    }]
+    d = simulate_data(n,
+                      outcome_prevalence = c(0.1,0.2),
+                      death_prevalence = c(0.1),
+                      censoring_probability = c(0.2),
+                      effects = list("A1" = c(0,0,0),
+                                     "B1" = c(0,0,0),
+                                     "L1" = c(0,0,0),
+                                     "Y1" = c(0,0,0),
+                                     "C1" = c(0,0,0),
+                                     "D1" = c(0,0,0),
+                                     "Y2" = c(0,0,0,0,0,0)))
+    
 }
 
 
--- simulation/randomized_treatment/functions/simulate_data.R
+++ simulation/randomized_treatment/functions/simulate_data.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Sep 12 2023 (14:24) 
 ## Version: 
-## Last-Updated: Sep 13 2023 (11:16) 
+## Last-Updated: Oct 16 2023 (16:00) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 12
+##     Update #: 19
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -17,24 +17,38 @@
 ## 
 ### Code:
 simulate_data <- function(n,
-                          outcome_prevalence = 0.2,
-                          treatment_effect, 
-                          covariate_effect = c(0,0)){
-    m <- lava::lvm(~Y_treated+Y_untreated+A+W)
-    lava::distribution(m,~Y_treated+Y_untreated+A+W) <- lava::binomial.lvm()
+                          outcome_prevalence = c(0.1,0.2),
+                          death_prevalence = c(0.1),
+                          censoring_probability = c(0.2),
+                          effects = list(
+                              # effects are W,L0,A0
+                              "A1" = c(0,0,0),
+                              "B1" = c(0,0,0),
+                              "L1" = c(0,0,0),
+                              "Y1" = c(0,0,0),
+                              "C1" = c(0,0,0),
+                              "D1" = c(0,0,0),
+                              # effects are W,L0,A0,L1,A1,B1
+                              "Y2" = c(0,0,0,0,0,0))){
+    # 
+    # NOTE: A0 = 1-B0 hence we do not need to generate B0
+    # 
+    m <- lava::lvm(~Y2+Y1+D1+C1+A1+A0+B1+L1+L0+W)
+    lava::distribution(m,~Y2+Y1+D1+C1+A1+A0+B1+L1+L0+W) <- lava::binomial.lvm()
     # in absence of other variables the intercept
     # is on the logit scale such that if we specify
     # an intercept of logit(0.8) then the risk = P(Y=1) is 0.8
-    lava::intercept(m,~Y_untreated) <- lava::logit(outcome_prevalence)
-    # the treatment effect is specified as a risk difference
-    if (treatment_effect>outcome_prevalence){stop("Treatment effect is larger than risk of the outcome.")}
-    lava::intercept(m,~Y_treated) <- logit(outcome_prevalence-treatment_effect)
-    lava::regression(m,Y_treated~W) <- covariate_effect[[1]]
-    lava::regression(m,Y_untreated~W) <- covariate_effect[[1]]
-    lava::regression(m,A~W) <- covariate_effect[[2]]
-    lava::transform(m,Y~Y_treated+Y_untreated+A) = function(x){
-        x[["Y_treated"]]*(x[["A"]] == 1)+ x[["Y_untreated"]]*(x[["A"]] == 0)
-    }
+    lava::intercept(m,~Y1) <- lava::logit(outcome_prevalence[[1]])
+    lava::intercept(m,~Y2) <- lava::logit(outcome_prevalence[[2]])
+    lava::intercept(m,~C1) <- lava::logit(censoring_probability[[1]])
+    lava::intercept(m,~D1) <- lava::logit(death_prevalence[[1]])
+    lava::regression(m,L1~W+L0+A0) <- effects[["Y1"]]
+    lava::regression(m,Y1~W+L0+A0) <- effects[["Y1"]]
+    lava::regression(m,D1~W+L0+A0) <- effects[["D1"]]
+    lava::regression(m,C1~W+L0+A0) <- effects[["C1"]]
+    lava::regression(m,Y2~W+L0+A0+L1+A1+B1) <- effects[["Y2"]]
+    lava::regression(m,A1~W+L0+A0) <- effects[["A1"]]
+    lava::regression(m,B1~W+L0+A0) <- effects[["B1"]]
     d = lava::sim(m,n)
     setDT(d)
     d
