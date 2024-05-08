0d68554b2ad0c2e31e8c3d97324cf7ebdb442acb
Thomas Alexander Gerds <tag@biostat.ku.dk>
2024-05-08 08:43:17 +0200

    poly run

---
+++ Ltmle/R/augmentation_show_performance.R
@@ -0,0 +1,32 @@
+### show_performance.R ---
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: Dec 10 2023 (07:01)
+## Version:
+## Last-Updated: feb 20 2024 (11:13) 
+##           By: WJA3740
+##     Update #: 14
+#----------------------------------------------------------------------
+##
+### Commentary:
+##
+### Change Log:
+#----------------------------------------------------------------------
+##
+### Code:
+show_performance <- function(print=TRUE){
+        m = tar_meta()
+            data.table::setDT(m)
+            m = m[type == "stem"]
+            m[,Memory := utils:::format.object_size(bytes,units="GB")]
+            m[,Minutes := seconds%/%(60)]
+            m[,Hours := seconds%/%(60*60)]
+            m = m[,.(Target = name,Hours,Minutes,Seconds = seconds,Memory,Storage = format)]
+            if (all(m$Hours == 0)) m$Hours = NULL else m$Seconds = NULL
+            if (print) print(m)
+            invisible(m)
+        }
+######################################################################
+### show_performance.R ends here
+
+
